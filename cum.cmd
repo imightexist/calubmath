512e60c4060a9870e75d304d33f4c287d975ad17
imightexist <94249457+imightexist@users.noreply.github.com>
2026-08-17 20:32:48 -0500

    Add files via upload

---
+++ cum.cmd
@@ -0,0 +1,18 @@
+@echo off
+pushd %~dp0%
+noclose
+if "%1"=="skid" goto init
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoClose /t REG_DWORD /d 1 /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /d "cmd /c cd /d %cd% && cum skid" /f
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v shutdownwithoutlogon /t REG_DWORD /d 0 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+shutdown -l
+exit
+:init
+python314\python skid.py
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoClose /f
+reg delete "HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /f
+reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v shutdownwithoutlogon /f
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /f
+start userinit
+exit
---
+++ skid.py
@@ -0,0 +1,61 @@
+from signal import signal, SIGINT, SIG_IGN
+from json import load
+from random import choice
+import subprocess
+
+signal(SIGINT, SIG_IGN)
+
+eng = None
+math = None
+with open("eng.json", "rb") as f:
+    eng = load(f)
+with open("math.json", "rb") as f:
+    math = load(f)
+
+done = []
+correct = 0
+def getQuestion(array):
+    q = None
+    while (q is None) or (q["id"] in done):
+        q = choice(array)
+    return q
+
+def doQuestion(array):
+    global done
+    global correct
+    q = getQuestion(array)
+    qd = q["question"]
+    if qd["paragraph"] != "null":
+        print(f"\"{qd["paragraph"]}\"")
+        print()
+    print(f"Q: {qd["question"]}")
+    for answer in qd["choices"].keys():
+        text = qd["choices"][answer]
+        print(f"\t{answer} -> {text}")
+    inp = None
+    while inp is None:
+        try:
+            inp = input("A: ").strip().upper()[0]
+        except:
+            print()
+    if inp == qd["correct_answer"]:
+        correct += 1
+        print("Correct answer! "+str(correct)+"/6")
+        done.append(q["id"])
+    else:
+        correct = 0
+        print("WRONG! "+str(correct)+"/6")
+        print("Correct answer: " + qd["correct_answer"])
+        done = []
+    print()
+    print()
+    print()
+print("calubSAT")
+print("Get 6 questions correct in a row to get access to Windows")
+print()
+
+while correct < 6:
+    doQuestion(eng)
+    doQuestion(math)
+
+print("Very well. Come back soon!")
