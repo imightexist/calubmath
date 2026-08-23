fd4e1d7669427d238e18e402f9329afe8c99727c
imightexist <94249457+imightexist@users.noreply.github.com>
2026-08-22 19:37:19 -0500

    Update anticheat.py

--- anticheat.py
+++ anticheat.py
@@ -2,9 +2,9 @@ from signal import signal, SIGINT, SIG_IGN
 import subprocess
 signal(SIGINT, SIG_IGN)
 while True:
-  subprocess.Popen(["taskkill","/f","/im","chrome.exe"]).wait()
-  subprocess.Popen(["taskkill","/f","/im","msedge.exe"]).wait()
-  subprocess.Popen(["taskkill","/f","/im","plasmafox.exe"]).wait()
-  subprocess.Popen(["taskkill","/f","/im","firefox.exe"]).wait()
-  subprocess.Popen(["taskkill","/f","/im","iexplore.exe"]).wait()
-  subprocess.Popen(["taskkill","/f","/im","r3dfox.exe"]).wait()
+  subprocess.Popen(["taskkill","/f","/im","chrome.exe"], creationflags=0x08000000).wait()
+  subprocess.Popen(["taskkill","/f","/im","msedge.exe"], creationflags=0x08000000).wait()
+  subprocess.Popen(["taskkill","/f","/im","plasmafox.exe"], creationflags=0x08000000).wait()
+  subprocess.Popen(["taskkill","/f","/im","firefox.exe"], creationflags=0x08000000).wait()
+  subprocess.Popen(["taskkill","/f","/im","iexplore.exe"], creationflags=0x08000000).wait()
+  subprocess.Popen(["taskkill","/f","/im","r3dfox.exe"], creationflags=0x08000000).wait()
