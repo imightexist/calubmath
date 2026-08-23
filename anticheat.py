88493ba48188a5612f2ceb8cfd7a0b421732840c
imightexist <94249457+imightexist@users.noreply.github.com>
2026-08-23 08:56:17 -0500

    Update anticheat.py

--- anticheat.py
+++ anticheat.py
@@ -1,5 +1,6 @@
 from signal import signal, SIGINT, SIG_IGN
 import subprocess
+import time
 signal(SIGINT, SIG_IGN)
 while True:
   subprocess.Popen(["taskkill","/f","/im","chrome.exe"], creationflags=0x08000000).wait()
@@ -8,3 +9,4 @@ while True:
   subprocess.Popen(["taskkill","/f","/im","firefox.exe"], creationflags=0x08000000).wait()
   subprocess.Popen(["taskkill","/f","/im","iexplore.exe"], creationflags=0x08000000).wait()
   subprocess.Popen(["taskkill","/f","/im","r3dfox.exe"], creationflags=0x08000000).wait()
+  time.sleep(1)
